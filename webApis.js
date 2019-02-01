153d387410e96ff47428f1932700db40b9b81733
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2019-02-01 19:08:01 +0530

    Added WebApis Execution

---
+++ webApis.js
@@ -0,0 +1,23 @@
+// Try executing this with "Loupe"
+
+var userName = "Mayank";
+
+// These "setTimeout" is executed on Web APIs..
+
+setTimeout(function(){
+    userName = "Meha";
+}, 1000);
+
+setTimeout(function(){
+    userName = "Meha";
+}, 1000);
+
+setTimeout(function(){
+    userName = "Meha";
+}, 1000);
+
+var age = 10;
+
+var designation = "Developer";
+
+// http://latentflip.com/loupe/?code=c2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoK!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
